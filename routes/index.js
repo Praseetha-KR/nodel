03b5f0f81c78696ab5eef48f4179cdd810f7926b
Praseetha-KR <voidimagineer@yahoo.com>
2014-10-16 20:07:35 +0530

    Added gitignore, print hello world

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/hello', function(req, res) {
+	res.render('hello', { title: 'Hello World!' });
+});
+
 module.exports = router;
